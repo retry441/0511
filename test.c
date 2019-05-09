dcab12d03c279a53ad14e92b6094619dcac589d2
retry441 <yan52811648@outlook.com>
2019-05-09 21:19:10 +0800

    首次上传至版本库，有两个文件，为版本1

---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main()
+{
+    int i;
+    i = 1;
+    printf("这是此程序的第%d个版本\n",i);
+}
---
+++ test2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("第二个c文件，多个文件被add进入暂存区以后，可以一次性被commit进入版本库\n");
+}
